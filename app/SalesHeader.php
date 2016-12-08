57009ab8a71e574686c6064add3fdb49d4a0582f
chaturanga836 <chaturanga836@gmail.com>
2016-12-08 07:34:00 +0530

    xcxxcx
    cvcv

---
+++ app/SalesHeader.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use DB;
+use DateTime;
+class SalesHeader extends Model{
+
+  protected $table = 'SalesHeader';
+
+ public $primaryKey='IntNo';
+ public $timestamps = false;
+}
